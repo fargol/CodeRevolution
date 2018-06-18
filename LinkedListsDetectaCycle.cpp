3eaa03697b9bf2b497de96e61db4d5fffbf4b628
Siddharth Thakur <codeblooded29@gmail.com>
2018-06-18 17:51:20 +0530

    Hackerrank

---
+++ LinkedListsDetectaCycle.cpp
@@ -0,0 +1,26 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+    // Complete this function
+    // Do not write the main method
+    if(head==NULL)
+        return false;
+    int mark[100000]={1};
+    Node* curr=head;
+    while(curr!=NULL){
+        if(mark[curr->data])
+            mark[curr->data]=0;
+        else
+            return true;
+        curr=curr->next;
+    }
+    return false;
+}
