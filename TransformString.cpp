6502ab047ce44b2041aebe290f5e2249ccdce38a
Siddharth Thakur <codeblooded29@gmail.com>
2018-07-28 00:11:27 +0530

    GeeksforGeeks

--- TransformString.cpp
+++ TransformString.cpp
@@ -9,25 +9,35 @@ int main(){
 	int T;
 	string A,B;
 	cin>>T;
-	int** dp;
-	dp=(int**)calloc(100010,sizeof(int*));
-	for(int i=0;i<100010;i++)
-		dp[i]=(int*)calloc(100010,sizeof(int));
-	//vector<vector<int> > dp(100010,vector<int>(100010));
+	int count;
+	int Al,Bl;
 	while(T--){
 		cin>>A>>B;
-		for(int i=0;i<=A.length();i++)
-			dp[i][0]=i;
-		for(int j=0;j<=B.length();j++)
-			dp[0][j]=j;
-		for(int i=1;i<=A.length();i++){
-			for(int j=1;j<=B.length();j++){
-				if(A[i-1]==B[j-1])
-					dp[i][j]=dp[i-1][j-1];
-				else
-					dp[i][j]=min(dp[i-1][j],dp[i][j-1])+1;
+		set<char> As,Bs;
+		Al=A.length();
+		Bl=B.length();
+		if(Al!=Bl){
+			cout<<-1<<endl;
+			continue;
+		}
+		for(int i=0;i<Al;i++)
+			As.insert(A[i]);
+		for(int i=0;i<Bl;i++)
+			Bs.insert(B[i]);
+		if(As!=Bs){
+			cout<<-1<<endl;
+			continue;
+		}
+		int j=Al-1;
+		count=0;
+		for(int i=Al-1;i>=0;i--){
+			if(A[i]==B[j]){
+				j--;
+			}
+			else{
+				count++;
 			}
 		}
-		cout<<(dp[A.length()][B.length()]/2)<<endl;
+		cout<<count<<endl;
 	}
 }
