2260ca96ea89c3fce5f6939d3e9cb0a9f11626dc
Siddharth Thakur <codeblooded29@gmail.com>
2018-08-22 01:51:44 +0530

    GeeksforGeeks

--- LongestvalidParentheses.cpp
+++ LongestvalidParentheses.cpp
@@ -3,33 +3,26 @@ using namespace std;
 #define endl '\n';
 int main(){
 	ios::sync_with_stdio(true);
-	int T;
+	int T,maxl,c;
 	cin>>T;
 	string S;
-	int h,p,flag,max;
 	while(T--){
 		cin>>S;
-		h=0;
-		p=0;
-		//flag=0;
-		max=INT_MIN;
+		stack<char> brac;
+		brac.push(-1);
+		maxl=INT_MIN;
 		for(int i=0;i<S.length();i++){
-			if(S[i]=='('){
-				h++;
-				//flag=1;
-			}
-			else if(h>0){
-				h--;
-				p++;
-			}
-			if(h==0){
-				if(max<p)
-					max=p;
-				p=0;
+			if(S[i]=='(')
+				brac.push(i);
+			else{
+				brac.pop();
+				if(!brac.empty()){
+					maxl=max(i-brac.top(),maxl);
+				}
+				else
+					brac.push(i);
 			}
 		}
-		if(max<p)
-			max=p;
-		cout<<2*max<<endl;
+		cout<<maxl<<endl;
 	}
 }
