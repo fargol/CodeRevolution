dca3422c6d99719e306c7edc679bd789fa0ebfd6
Siddharth Thakur <codeblooded29@gmail.com>
2018-09-30 00:05:50 +0530

    Update

--- WordBreak.cpp
+++ WordBreak.cpp
@@ -18,17 +18,24 @@ int main(){
 		}
 		cin>>S;
 		SS="";
+		bool dp[S.length()+1]={false};
+		vector<int> last_match;
+		last_match.push_back(-1);
 		for(int i=0;i<S.length();i++){
-			SS+=S[i];
-			if(ump.find(SS)!=ump.end()){
-				SS="";
+			int msize=last_match.size();
+			bool flag=false;
+			for(int j=msize-1;j>=0;j--){
+				SS=S.substr(last_match[j]+1,i-last_match[j]);
+				if(ump.find(SS)!=ump.end()){
+					flag=true;
+					break;
+				}
+			}
+			if(flag){
+				dp[i]=true;
+				last_match.push_back(i);
 			}
 		}
-		if(SS!=""){
-			cout<<0<<endl;
-		}
-		else{
-			cout<<1<<endl;
-		}
+		cout<<dp[S.length()-1]<<endl;
 	}
 }
