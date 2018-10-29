c6ff5ccea9daf59f3c9206b3bb2e67511563272a
Siddharth Thakur <codeblooded29@gmail.com>
2018-10-29 23:30:40 +0530

    DP Problem

--- InterleavedStrings.cpp
+++ InterleavedStrings.cpp
@@ -1,22 +1,20 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 bool isInterleave(string A, string B, string C);
 int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		string a,b,c;
-		cin>>a;
-		cin>>b;
-		cin>>c;
-		cout<<isInterleave(a,b,c)<<endl;
-	}
-	// your code goes here
-	return 0;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a,b,c;
+        cin>>a;
+        cin>>b;
+        cin>>c;
+        cout<<isInterleave(a,b,c)<<endl;
+    }
+    // your code goes here
+    return 0;
 }
-
 /*Please note that it's Function problem i.e.
 you need to write your solution in the form of Function(s) only.
 Driver Code to call/invoke your function is mentioned above.*/
@@ -25,31 +23,25 @@ Driver Code to call/invoke your function is mentioned above.*/
 bool isInterleave(string A, string B, string C) 
 {
     //Your code here
-    int flag1=0,flag2=0;
-    int ca=0,cb=0,cc=0;
-    for(int i=0;i<C.length();i++){
-        if(C[i]==A[ca])
-            ca++;
-        else if(C[i]==B[cb])
-            cb++;
-        else{
-            flag1=1;
-            break;
-        }
-    }
-    ca=cb=0;
-    for(int i=0;i<C.length();i++){
-        if(C[i]==B[cb])
-            cb++;
-        else if(C[i]==A[ca])
-            ca++;
-        else{
-            flag2=1;
-            break;
-        }
-    }
-    if(!flag1||!flag2)
-        return true;
-    else
+    if(C.length()!=A.length()+B.length())
         return false;
+    bool dp[A.length()+1][B.length()+1];
+    for(int i=0;i<=A.length();i++){
+        for(int j=0;j<=B.length();j++){
+            dp[i][j]=false;
+            if(i==0&&j==0)
+                dp[i][j]=true;
+            else if(i==0&&C[i+j-1]==B[j-1])
+                dp[i][j]=dp[i][j-1];
+            else if(j==0&&C[i-1]==A[i-1])
+                dp[i][j]=dp[i-1][j];
+            else if(A[i-1]==C[i+j-1]&&B[j-1]!=C[i+j-1])
+                dp[i][j]=dp[i-1][j];
+            else if(A[i-1]!=C[i+j-1]&&B[j-1]==C[i+j-1])
+                dp[i][j]=dp[i][j-1];
+            else if(A[i-1]==C[i+j-1]&&B[j-1]==C[i+j-1])
+                dp[i][j]=(dp[i-1][j]|dp[i][j-1]);
+        }
+    }
+    return dp[A.length()][B.length()];
 }
