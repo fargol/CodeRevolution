ffcdf07eafe1e8f482a517c27caf612cf88e153b
Siddharth Thakur <codeblooded29@gmail.com>
2019-01-20 13:32:17 +0530

    Add files via upload

--- LongestIncreasingSubsequence.cpp
+++ LongestIncreasingSubsequence.cpp
@@ -1,33 +1,16 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n';
-typedef pair<int, int> ii; typedef vector<ii> vii;
-int main(){
-	//vector<vii> AdjList;
-	//vector< pair<int, ii> > EdgeList;
-	ios::sync_with_stdio(true);
-	int T,N;
-	int arr[1010];
-	int dp[1010];
-	cin>>T;
-	int maxDp;
-	while(T--){
-		cin>>N;
-		for(int i=0;i<N;i++)
-			cin>>arr[i];
-		dp[0]=1;
-		maxDp=1;
-		for(int i=1;i<N;i++){
-			dp[i]=1;
-			for(int j=0;j<i;j++){
-				if(arr[j]<arr[i]){
-					if(dp[i]<dp[j]+1)
-						dp[i]=dp[j]+1;
-				}
-			}
-			if(maxDp<dp[i])
-				maxDp=dp[i];
-		}
-		cout<<maxDp<<endl;
-	}
+int Solution::lis(const vector<int> &A) {
+    int N=A.size();
+    vector<int> dp(N);
+    dp[0]=1;
+    int res=1;
+    for(int i=1;i<N;i++){
+        dp[i]=1;
+        for(int j=0;j<i;j++){
+            if(A[j]<A[i]){
+                dp[i]=max(dp[i],dp[j]+1);
+            }
+        }
+        res=max(dp[i],res);
+    }
+    return res;
 }
