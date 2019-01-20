3bc64a5f88cc5eeb11fab18f1d4220f1d2b0fb6a
Siddharth Thakur <codeblooded29@gmail.com>
2019-01-20 13:05:11 +0530

    DP problem

--- LongestArithmeticProgression.cpp
+++ LongestArithmeticProgression.cpp
@@ -1,51 +1,29 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n';
-#define M 1000000007;
-typedef pair<int, int> ii; typedef vector<ii> vii;
-int main(){
-	//vector<vii> AdjList;
-	//vector< pair<int, ii> > EdgeList;
-	ios::sync_with_stdio(true);
-	int T,N,maxllap;
-	int arr[4999];
-	vector<vector<int> > dp(4999,vector<int>(4999));
-	cin>>T;
-	while(T--){
-		cin>>N;
-		for(int i=0;i<N;i++)
-			cin>>arr[i];
-		for(int i=0;i<N-1;i++){
-			dp[i][N-1]=2;
-			dp[i][i]=1;
-		}
-		if(N<2)
-		    maxllap=1;
-		else
-		    maxllap=2;
-		for(int j=N-2;j>=1;j--){
-			int i=j-1,k=j+1;
-			while(i>=0&&k<N){
-				if(arr[i]+arr[k]==2*arr[j]){
-					dp[i][j]=dp[j][k]+1;
-					maxllap=max(maxllap,dp[i][j]);
-					i--;
-					k++;
-				}
-				else if(arr[i]+arr[k]<2*arr[j]){
-					k++;
-				}
-				else{
-				    dp[i][j]=2;
-					i--;
-				}
-			}
-			while (i >= 0)
-            {
-                dp[i][j] = 2;
-                i--;
+int Solution::solve(const vector<int> &A) {
+    int N=A.size();
+    if(N<3){
+        return N;
+    }
+    vector<vector<int> > dp(N,vector<int>(N,0));
+    unordered_map<int,int> hm;
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            dp[i][j]=2;
+            int diff=A[j]-A[i];
+            int x=2*A[i]-A[j];
+            if(hm.count(x)==0){
+                continue;
             }
-		}
-		cout<<maxllap<<endl;
-	}	
+            else{
+                dp[i][j]=max(dp[i][j],dp[hm[x]][i]+1);
+            }
+        }
+        hm[A[i]]=i;
+    }
+    int ans=2;
+    for(int i=0;i<N;i++){
+        for(int j=i+1;j<N;j++){
+            ans=max(ans,dp[i][j]);
+        }
+    }
+    return ans;
 }
